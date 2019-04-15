9788e6a0b0dcad1b5098fc93185c3fc0c7b29652
Allen Welch <allenjwelch@gmail.com>
2019-04-14 21:22:20 -0400

    updated with projects page

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,6 +2,7 @@ const onScroll = () => {
     console.log('testing')
     const wh = window.innerHeight;
     const headerElem = document.querySelector('.header'); 
+    console.log(headerElem)
     
     let textLinks; 
     let homeKickers; 
@@ -10,10 +11,30 @@ const onScroll = () => {
         textLinks = document.querySelectorAll('.text-link'); 
         homeKickers = document.querySelectorAll('.kicker'); 
         console.log(homeKickers)
+        console.log(textLinks)
     }
     
-    console.log('wh: ', wh); 
-    // console.log(headerElem); 
+     //---- PROJECT SCROLL ----
+    if(document.querySelector('.projects-page')) {
+        console.log('projects-page')
+        // document.getElementById('project1-tile').addEventListener('click', () => {
+        //     console.log('click')
+        // })
+
+        let projects = document.querySelectorAll('.project-tile')
+        projects.forEach(project => {
+            project.addEventListener('click', function() {
+                let projectDiv = document.querySelector(`.project[data-project="${this.dataset.project}"]`)
+                console.log(projectDiv)
+                // projectDiv.scrollIntoView(false);
+                projectDiv.scrollIntoView({ block: "center", behavior: 'smooth' });
+
+
+            })
+        })
+        
+        
+    }
     
     window.addEventListener('scroll', function(e) {
         let scrollPosLow = window.scrollY + (wh * .66); 
@@ -22,24 +43,23 @@ const onScroll = () => {
 
 
         //---- HEADER SCROLL ----
-        window.scrollY > 200 ? headerElem.classList.add('scrolled') : headerElem.classList.remove('scrolled')
+        window.scrollY > 200 ? document.querySelector('.header').classList.add('scrolled') : document.querySelector('.header').classList.remove('scrolled')
 
         //---- HOME SCROLL ----
         if(document.querySelector('.home-page')) {
             // console.log('link.offsetTop: ', textLink[0].offsetTop)
             // console.log('scrollPosCenter: ', scrollPosCenter)
 
-            textLinks.forEach( (link, i) => {
+            document.querySelectorAll('.text-link').forEach( (link, i) => {
                 scrollPosCenter >= link.offsetTop ? link.classList.add('scrolled') : link.classList.remove('scrolled'); 
             })
 
-            homeKickers.forEach( (kicker, i) => {
+            document.querySelectorAll('.kicker').forEach( (kicker, i) => {
                 scrollPosLow >= kicker.offsetTop ? kicker.classList.add('scrolled') : kicker.classList.remove('scrolled'); 
             })
-
-            
-
         }
+
+       
     })
 }
 
