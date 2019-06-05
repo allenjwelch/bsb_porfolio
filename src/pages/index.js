7252a4d00f56605d809017e829875ef1f9121636
Allen Welch <allenjwelch@gmail.com>
2019-06-04 21:39:59 -0400

    added recycle page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,30 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Chart from 'chart.js';
-import './css/index.css'
+import './css/index.scss'
+
+import qoinsLogo from '../images/qoins/title1.png'
+import qoinsLogo2 from '../images/qoins/title2.png'
+import recycleLogo from '../images/recycleatl/logo.png'
+import recycleLogo2 from '../images/recycleatl/logo2.png'
 
 class Home extends React.Component {
 
 	componentDidMount() {
-		this.chartjs()
-		this.homeScroll()
+        this.projectScroll();
 	}
 
-	homeScroll() {
-		const wh = window.innerHeight;
-		let textLinks = document.querySelectorAll('.text-link'); 
-		let homeKickers = document.querySelectorAll('.kicker');  
-	
-		window.addEventListener('scroll', function(e) {
-			let scrollPosLow = window.scrollY + (wh * .66); 
-			let scrollPosCenter = window.scrollY + (wh /2); 
-			
-			textLinks.forEach( (link, i) => {
-                scrollPosCenter >= link.offsetTop ? link.classList.add('scrolled') : link.classList.remove('scrolled'); 
+	projectScroll() {
+        const projects = document.querySelectorAll('.project-tile')
+        projects.forEach(project => {
+            project.addEventListener('click', function() {
+                let projectDiv = document.querySelector(`.project[data-project="${this.dataset.project}"]`)
+                console.log(projectDiv)
+                // projectDiv.scrollIntoView(false);
+                projectDiv.scrollIntoView({ block: "center", behavior: 'smooth' });
             })
-
-            homeKickers.forEach( (kicker, i) => {
-                scrollPosLow >= kicker.offsetTop ? kicker.classList.add('scrolled') : kicker.classList.remove('scrolled'); 
-            })
-		})
-	}
-
-	chartjs() {
-		const ctx = document.getElementById('life-chart');
-		// eslint-disable-next-line
-		let doughnutChart = new Chart(ctx, {
-			type: "doughnut",
-			data: {
-				labels: ["this", "that", "the other", "more stuff", "other stuff"],
-				datasets: [
-					{
-						label: "These things...",
-						data: [12, 19, 3, 14, 7],
-						backgroundColor: ['rgba(0, 150, 0, .5)'],
-					},
-				]
-			}
-		});
-	}
+        })
+    }
 
 
 	render() {
@@ -62,48 +40,77 @@ class Home extends React.Component {
 
 					<section className="content">
 						<section className="brief-text">
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, voluptate?</p>
+							<p>A creative thinker and artist dedicated to the user centered process of research and design</p>
+						</section>
+
+						<section className="project-tiles">
+							<img className="project-tile" data-project="1" src={qoinsLogo} alt=""/>
+							<img className="project-tile" data-project="2" src={recycleLogo2} alt=""/>
+							<img className="project-tile" data-project="3" src="https://via.placeholder.com/300C/O" alt=""/>
+							{/* <img className="project-tile" data-project="4" src="https://via.placeholder.com/300C/O" alt=""/>
+							<img className="project-tile" data-project="5" src="https://via.placeholder.com/300C/O" alt=""/>
+							<img className="project-tile" data-project="6" src="https://via.placeholder.com/300C/O" alt=""/> */}
 						</section>
 
 						<section className="intro-text">
 							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis ratione, tempora nisi perspiciatis illum officia fugiat explicabo, voluptatum sunt corporis similique deserunt accusantium itaque sed rerum et quidem? Fuga delectus quaerat qui iure quae laudantium provident maxime nam dignissimos. Animi <Link className="text-link" to="/projects/">projects</Link> saepe, magnam consequatur repudiandae illum eligendi, est corrupti dolores rerum modi vitae similique sapiente quod non laudantium omnis ullam iste molestiae at tenetur mollitia veniam. Accusamus corporis eius alias architecto sunt, maiores doloribus maxime officiis, nihil ducimus eos suscipit doloremque cumque voluptatum inventore quidem mollitia labore necessitatibus cum culpa? Iste voluptate minus, quos dignissimos earum atque tempora obcaecati minima.</p>
 						</section>
 
-						<section className="followup-text">
 
-							<div className="chart">
-								<canvas id="life-chart" height="400px" width="400px"></canvas>
-							</div>
 
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quaerat eum recusandae molestias! Aspernatur, iure. Ipsa, vel quae placeat saepe magni eligendi ab ex quas, magnam officiis tempora recusandae dolorem exercitationem sapiente doloribus expedita omnis totam vero possimus labore sunt odio. Doloribus <Link className="text-link" to="/art/">art</Link> perspiciatis incidunt possimus facere rerum porro, asperiores, laboriosam laborum sint reiciendis dolore eius a praesentium, dolorem similique et quos vitae enim. Labore unde consectetur qui inventore ipsa assumenda cumque sed repellendus sunt tenetur! Quos obcaecati quaerat aut.</p>
+						<section className="projects-container">
+							<article className="project" data-project="1">
+								<Link to="/qoins">
+									<div className="project-image">
+										<img src={qoinsLogo2} alt=""/>
+									</div>
+									<div className="project-description">
+										<h1>Qoins</h1>
+										<p>A financial management mobile app that rounds up your individual purchases and allows you to set up automatic payments to set money aside in order for you to pay off your debt faster</p>
+									</div>
+									{/* <div className="project-tags">
+										<p>role</p>
+										<p>features</p>
+										<p>other stuff</p>
+									</div> */}
+								</Link>
+							</article>
+
+							<article className="project" data-project="2">
+								<Link to="/recycleATL">
+
+									<div className="project-description">
+										<h1>Recycle ATL</h1>
+										<p>Recycle ATL is what we call a passion project.  It comes out of my need to know where I can specifically recycle glass, since the city of Atlanta no longer recycles glass.  Not only that, but also for users living in locations that do not offer curbside recycling.</p>
+									</div>
+									<div className="project-image">
+										<img src={recycleLogo} alt=""/>
+									</div>
+									{/* <div className="project-tags">
+										<p>role</p>
+										<p>features</p>
+										<p>other stuff</p>
+									</div> */}
+								</Link>
+							</article>
+
+							<article className="project"  data-project="3">
+								<div className="project-image">
+									<img src="https://via.placeholder.com/500x400" alt=""/>
+								</div>
+								<div className="project-description">
+									<h1>TITLE</h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla libero at dolor consequuntur dolores impedit pariatur recusandae maxime inventore praesentium iste sunt unde, iusto commodi esse nostrum aliquam beatae repellendus?</p>
+								</div>
+								{/* <div className="project-tags">
+									<p>role</p>
+									<p>features</p>
+									<p>other stuff</p>
+								</div> */}
+							</article>
 						</section>
 
-						<section className="skill-badges">
-							<div className="badge">
-								<img src="https://via.placeholder.com/150" alt=""/>
-								<p>Lorem ipsum dolor sit amet.</p>
-							</div>
-							<div className="badge">
-								<img src="https://via.placeholder.com/150" alt=""/>
-								<p>Lorem ipsum dolor sit amet.</p>
-							</div>
-							<div className="badge">
-								<img src="https://via.placeholder.com/150" alt=""/>
-								<p>Lorem ipsum dolor sit amet.</p>
-							</div>
-						</section>
 
-						<section className="bottom-kickers">
-							<div className="kicker" id="kicker1">
-								<img src="https://via.placeholder.com/300" alt=""/>
-								<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, quo?</p>
-							</div>
-							<div className="kicker" id="kicker2">
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, a.</p>
-								<img src="https://via.placeholder.com/300C/O" alt=""/>
-							</div>
-						</section>
-					
 					</section>
 				</div>
 			</Layout>
